e57680cf82bf1baac18b6f5136c584a56655183c
Shrawan Gorathoki <shrawan44425@gmail.com>
2024-06-15 14:03:31 +0545

    first commit

---
+++ qrgenerator/android/app/src/main/kotlin/com/example/qrgenerator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qrgenerator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ qrgenerator/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qrgenerator/QRPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Code Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: QRCodeGenerator(),
+    );
+  }
+}
+
+class QRCodeGenerator extends StatefulWidget {
+  @override
+  _QRCodeGeneratorState createState() => _QRCodeGeneratorState();
+}
+
+class _QRCodeGeneratorState extends State<QRCodeGenerator> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _vehicleNumberController = TextEditingController();
+
+  String? _qrData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Generator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Enter Name',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _vehicleNumberController,
+              decoration: InputDecoration(
+                hintText: 'Enter Vehicle Number',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _qrData = '${_nameController.text}\n${_vehicleNumberController.text}';
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => QRViewPage( name: _nameController.text, vehicleNum: _vehicleNumberController.text,))); 
+                });
+              },
+              child: Text('Generate QR Code'),
+            ),
+            SizedBox(height: 20),
+            // _qrData != null ? QrImageView(data: _qrData.toString(), version: QrVersions.auto, size: 200,) : SizedBox.shrink(), 
+            
+
+          ],
+        ),
+      ),
+    );
+  }
+}
